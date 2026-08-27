5d14a3944db61a180f6628aad32b8f7e06d6b582
Drenzzz <realdrenzzz@gmail.com>
2026-08-27 13:02:09 +0700

    chore: remove unused time event emission

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"embed"
 
 	"log"
-	"time"
 
 	"plumelauncher/internal/bootstrap"
 	"plumelauncher/internal/instances"
@@ -27,7 +26,6 @@ func init() {
 	// Register a custom event whose associated data type is string.
 	// This is not required, but the binding generator will pick up registered events
 	// and provide a strongly typed JS/TS API for them.
-	application.RegisterEvent[string]("time")
 	application.RegisterEvent[services.DownloadProgressEvent](services.EventDownloadProgress)
 	application.RegisterEvent[services.LaunchStateEvent](services.EventLaunchState)
 	application.RegisterEvent[services.LogLineEvent](services.EventLogLine)
@@ -125,16 +123,6 @@ func main() {
 		BackgroundColour: application.NewRGB(6, 7, 15),
 		URL:              "/",
 	})
-
-	// Create a goroutine that emits an event containing the current time every second.
-	// The frontend can listen to this event and update the UI accordingly.
-	go func() {
-		for {
-			now := time.Now().Format(time.RFC1123)
-			app.Event.Emit("time", now)
-			time.Sleep(time.Second)
-		}
-	}()
 
 	// Run the application. This blocks until the application has been exited.
 	err = app.Run()
